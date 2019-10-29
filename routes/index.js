1011090107f7276d02583212f7510809c46de215
BanlyTong <banly2602@gmail.com>
2019-10-29 12:22:59 +0700

    fix render README.md

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const fs = require('fs');
 const marked = require('marked');
+const path = require('path');
 const cores = require('./config.json');
 const Routers = require('./routers');
+const { logger } = require('./routers/helpers')
 
 module.exports = (app) => {
   /* GET home page will render README.md */
   app.get('/', (req, res, next) => {
-    const path = __dirname + '../../README.md';
-    
-    fs.readFile(path, 'utf8', (err, data) => {
+    const filePath = path.join(__dirname, '../README.md');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
       if (!err) {
         res.send(marked(data.toString()));
+      } else {
+        logger.error(err);
       }
     });
   });
@@ -20,8 +24,8 @@ module.exports = (app) => {
     const router = express.Router();
 
     core.routers.forEach(name => {
-      const params =  core.params ? core.params[name] : undefined;
-      router.use( Routers[name] (core.model, params) );
+      const params = core.params ? core.params[name] : undefined;
+      router.use(Routers[name](core.model, params));
     });
 
     app.use('/api/' + core.endpoint, router)
